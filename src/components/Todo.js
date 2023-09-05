1b100807f84a8bc51e7feb41532e934257f94768
MODALAVALASAPRAVEEN <praveenmodalavalasa@gmail.com>
2023-09-05 18:29:40 +0530

    Added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,26 @@
-import { Flex, Input } from "@chakra-ui/react";
-import { Center, Square, Circle } from "@chakra-ui/react";
+import { Input, Button, Heading, Center, Stack } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 import React from "react";
 
 const Todo = () => {
   return (
     <>
       <Center h="500px" color="white">
-          <Input placeholder="Basic usage" w="300px" />
+        <Stack>
+          <Stack direction="row" justifyContent="center">
+            <CheckIcon alignSelf="center" color="black" w="6" h="6" />
+            <Heading color="Black">Todo List</Heading>
+          </Stack>
+          <Stack direction="row">
+            <Input
+              placeholder="Add Item"
+              w="300px"
+              textColor="black"
+              marginRight="3"
+            />
+            <Button colorScheme="green">Add Item</Button>
+          </Stack>
+        </Stack>
       </Center>
     </>
   );
